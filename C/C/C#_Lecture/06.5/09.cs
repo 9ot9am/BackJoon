590a6806cc0350f9d9bc0e5de8114c1c260de9ce
9ot9am <9ot9am@gmail.com>
2025-04-11 16:54:32 +0900

    C#_Lecture : 9, 10

---
+++ C/C/C#_Lecture/06.5/09.cs
@@ -0,0 +1,22 @@
+// •임의의 정수 10개 중에, 100 미만인 숫자만 더해서 출력해보기
+// •ex)
+// 2, 99,207, 33, 102,39, 17, 997,2039, 5
+// -> 195
+
+    //임의의 int가 0개 들어간 배열 입력받기
+    //int [i] -> 가 < 100 인지 아닌지 판단하기
+    //if문 활용
+    //100 미만이면 sum에 더하기
+    //sum에 출력
+    
+    // int [] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    // int sum = 0;
+    //
+    // for (int i = 0; i < arr.Length; i++)
+    // {
+    //     if (arr[i] < 100)
+    //     {
+    //         sum += arr[i];
+    //     }
+    // }
+    // Console.WriteLine(sum);
---
+++ C/C/C#_Lecture/06.5/10.cs
@@ -0,0 +1,55 @@
+// •0~10억까지의 정수 N과 0~9까지의 정수 M이 주어졌을 때, N에서 M이 몇 번 나오는지 출력해보기
+
+//이거는 띄어쓰기가 있을 때
+
+    // int [] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    // int num = int.Parse(Console.ReadLine());
+    // int count = 0;
+    //
+    // for (int i = 0; i < arr.Length; i++)
+    // {
+    //     
+    //     if (arr[i] == num)
+    //     {
+    //         count++;
+    //     }
+    // }
+    // Console.WriteLine(count);
+
+//띄어쓰기가 없을 때
+
+    
+    //string s = Console.ReadLine();
+    //char ch = char.Parse(s);
+    
+    //string s = Console.ReadLine();
+    //char ch = Convert.ToChar(s);
+    
+    /*
+    string str = Console.ReadLine();
+    int count = 0;
+    
+    char ch = Console.ReadLine()[0]; // string의 첫 번째 배열은 char
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i] == ch)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+    
+    */
+
+
+
+//223712741 2 입력을 했을 때, 입력받을 수 있는 방법 
+
+    // string[] input = Console.ReadLine().Split(); // 공백 기준으로 나눔
+    //
+    // string str = input[0];           // "223712741"
+    // char target = input[1][0];       // '2'
+    //
+    
+    
